2aed13dc9c98d7e3488f3763896360dddd3788c7
VincONEent <ducatez.vincent@gmail.com>
2023-04-16 12:55:57 +0200

    Redign the UI

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Test to prove the scipt external file is linked to the html index page
-    console.log("Hello World");
-
-
-function getComputerChoice(){
+function getComputerChoice()
+{
     //Calling a Random number, the 3 max range set it up to get only 3
     // possibles options : 0 1 or 2
     // Each one of those will have a corresponding move in the game
@@ -10,114 +7,142 @@ function getComputerChoice(){
     let move;
     if(choice == 0)
     {
-        move ="rock";
+        move ="pierre";
     }
     else if(choice == 1)
     {
-        move="paper";
+        move="papier";
     }
     else if(choice == 2)
     {
-        move="cissors";
+        move="ciseaux";
     }
     return move;
-
 }
 
-function playRound(playerSelection, computerSelection){
 
-    let result = "Its a tie !"; // declaring a tie as the basis allow me to delete every tie event check, as if no-one won, its a tie
-    let gameResult ="gdg";
-    if(playerSelection.toLowerCase()=== "rock")
+function playRound(playerSelection, computerSelection)
+{
+    let result = ". C'est une egalite !"; // declaring a tie as the basis allow me to delete every tie event check, as if no-one won, its a tie
+    let gameResult ="";
+    nbr_round++;
+    if(playerSelection.toLowerCase()=== "pierre")
     {
-        if(computerSelection === "paper")
+        if(computerSelection === "papier")
         {
             //The computer won that round 
-            result = "The computer won : paper beat rock !";
+            result = ". L'ordinateur a gagne : papier bat la pierre ! ";
             computerScore++;
         }
-        else if(computerSelection === "cissors")
+        else if(computerSelection === "ciseaux")
         {
             // the player won that round
-            result = "The player won : rock beat cissors !";
+            result = ". Le joueur a gagne : la pierre bat les ciseaux ! ";
             playerScore++;
         }
     }
-    else if(playerSelection.toLowerCase()=== "paper")
+    else if(playerSelection.toLowerCase()=== "papier")
     {
-        if(computerSelection === "rock")
+        if(computerSelection === "pierre")
         {
             // the player won that round
-            result = "The player won : paper beat rock !";
+            result = ". Le joueur a gagne : le papier bat la pierre ! ";
             playerScore++;
         }
-        else if(computerSelection === "cissors")
+        else if(computerSelection === "ciseaux")
         {
             //The computer won that round
-            result = "The computer won : cissors beat paper !";
+            result = ". L'ordinateur a gagne : les ciseaux battent le papier ! ";
             computerScore++;
         }
     }
-    else if(playerSelection.toLowerCase()=== "cissors")
+    else if(playerSelection.toLowerCase()=== "ciseaux")
     {
-        if(computerSelection === "rock")
+        if(computerSelection === "pierre")
         {
-            result = "The computer won : rock beat cissors";
+            result = ". L'ordinateur a gagne : la pierre bat les ciseaux ! ";
             computerScore++;
             // the computer won that round
         }
-        else if(computerSelection === "paper")
+        else if(computerSelection === "papier")
         { 
-            result ="The player won : cissors beat paper";
+            result =". Le joueur a gagne : les ciseaux battent le papier ! ";
             playerScore++;
             //the player won that round
         }
     }
-    gameResult = "The computer played : " + computerSelection +"\n" + "The player played : " + playerSelection + "\n" + result + "\n";
+    gameResult = "Le joueur a joue : " + playerSelection + ". L'ordinateur a joue : " + computerSelection  + result ;
     return gameResult;
 }
 
-let computerScore = 0 ;
-let playerScore = 0 ;
-
+// a function that play one round
 function oneRound(playerChoice){
-    let resultat ="";
-    const computerMove = getComputerChoice();
-    resultat=playRound(playerChoice, computerMove);
-    Result.textContent= resultat;
 
+    let computerChoice = getComputerChoice();
+    actualisation(playRound(playerChoice, computerChoice)) ;
+    
+    if(computerScore === 5)
+    {
+    //The computer won
+        annoncement.textContent = "L'ordinateur a GAGNE ! Essaye encore !";
+        lock(true);
+
+    }
+    if(playerScore === 5)
+    {
+        annoncement.textContent = "Tu as GAGNE ! Felicitations! "
+        lock(true);
+    }
+}
+// a function that display the changes of the game state in the webPage
+function actualisation (annonce){
+     //the annonce is gonna be displayed in the annoncement section of the webpage
+    annoncement.textContent = annonce;
+    player.textContent = "Score du joueur : " + playerScore;
+    computer.textContent = "Score de l'adversaire : " + computerScore;
+    round.textContent = "Nombre de rounds : " +nbr_round;
 }
 
-//Associating the JS to the the html file, to allow the javaScript to modify element of the html page
-const buttons= document.querySelector('#Buttons');
-const Games= document.querySelector('#Games');
+//Remet tout la page a l etat initial, pret pour une nouvelle partie
+function reboot(){
+    computerScore = 0;
+    playerScore = 0;
+    nbr_round =0 ;
+    actualisation("La partie n'a pas pas encore commence ");
+    lock(false);
+}
+
+//If true  is send, the 3play button become unclickable, if else is send they get unlocked
+function lock(cadena){
+    rock.disabled = cadena;
+    paper.disabled = cadena;
+    scissors.disabled = cadena;
+}
 
 
-// Creating each indivual buttons
-const Paper = document.createElement('button');
-Paper.textContent = 'Paper';
-
-const Rock = document.createElement('button');
-Rock.textContent='Rock';
-
-const Cissors = document.createElement('button');
-Cissors.textContent='Cissors';
-
-// Apppending each button to the DIV associated to it in the html; the buttons will now appear
-buttons.appendChild(Rock);
-buttons.appendChild(Paper);
-buttons.appendChild(Cissors);
+let computerScore = 0 ;
+let playerScore = 0 ;
+let nbr_round = 0 ;
+let udie = 0;
 
 
-//Creationg the result part of the JS to display them in the html
-const Result = document.createElement('div');
-Result.textContent="The games hasn't started yet";
+// Connecting the javascript to the 3 buttons
+const rock = document.querySelector('#rockSelection');
+const paper = document.querySelector('#paperSelection');
+const scissors = document.querySelector('#scissorsSelection');
+const recommencer = document.querySelector('#reStart');
 
-Rock.addEventListener("click",(e) => oneRound('rock'));
-Paper.addEventListener("click",(e) => oneRound('paper'));
-Cissors.addEventListener("click",(e) => oneRound('cissors'));
+//Connecting the javascript to the 4 informations display pannels
+const player = document.querySelector('#joueur');
+const round = document.querySelector('#manche');
+const computer = document.querySelector('#ordinateur');
+const annoncement = document.querySelector('#resultat');
 
 
-//Appending the rule in the display Games div available in the HTML
-Games.appendChild(Result);
+
+rock.addEventListener('click', (e) => oneRound("pierre"));
+paper.addEventListener('click',(e) => oneRound("papier"));
+scissors.addEventListener('click',(e) => oneRound("ciseaux"));
+
+recommencer.addEventListener('click', (e) => reboot());
 
